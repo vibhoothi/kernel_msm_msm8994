36d8455f2d9334307044ac06494871954b7667c0
ShreyanshLodha <slodha96@gmail.com>
2017-09-29 23:35:44 +0530

    Revert "sound: codecs: Rename broken sysfs nodes"
    This reverts commit 2786f9a891bce6772b09cb8478507fb4e55fecef.

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -98,7 +98,7 @@ static ssize_t speaker_gain_show(struct kobject *kobj,
 {
         return sprintf(buf, "%u %u",
 			tomtom_read(fauxsound_codec_ptr,
-				TOMTOM_A_CDC_RX3_VOL_CTL_B2_CTL),
+				TOMTOM_A_CDC_RX7_VOL_CTL_B2_CTL),
 			tomtom_read(fauxsound_codec_ptr,
 				TOMTOM_A_CDC_RX7_VOL_CTL_B2_CTL));
 
@@ -113,7 +113,7 @@ static ssize_t speaker_gain_store(struct kobject *kobj,
 
 	if (calc_checksum(lval, rval, chksum)) {
 		tomtom_write(fauxsound_codec_ptr,
-			TOMTOM_A_CDC_RX3_VOL_CTL_B2_CTL, lval);
+			TOMTOM_A_CDC_RX7_VOL_CTL_B2_CTL, lval);
 		tomtom_write(fauxsound_codec_ptr,
 			TOMTOM_A_CDC_RX7_VOL_CTL_B2_CTL, rval);
 	}
@@ -191,13 +191,13 @@ static ssize_t sound_control_version_show(struct kobject *kobj, struct kobj_attr
 }
 
 static struct kobj_attribute cam_mic_gain_attribute =
-	__ATTR(gpl_cam_mic_gain_broken,
+	__ATTR(gpl_cam_mic_gain,
 		0666,
 		cam_mic_gain_show,
 		cam_mic_gain_store);
 
 static struct kobj_attribute mic_gain_attribute =
-	__ATTR(gpl_mic_gain_broken,
+	__ATTR(gpl_mic_gain,
 		0666,
 		mic_gain_show,
 		mic_gain_store);
